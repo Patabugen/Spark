460d4372fd00d1fa4f87cf7b674dcc1826e234cb
Patabugen <dude@patabugen.co.uk>
2010-10-12 10:22:19 +0100

    Added map heaad

---
+++ MapHead.php
@@ -0,0 +1,73 @@
+<?php
+function file_chop($file_path, $chunk_size){ 
+    $handle = @fopen($file_path, 'rb');         //read the file in binary mode 
+
+    if($handle == 0){
+	return false;
+    }
+    $size = filesize($file_path);  
+    $contents = fread($handle, $size); 
+    fclose($handle); 
+
+    //find number of full $chunk_size byte portions 
+    $num_chunks = floor($size/$chunk_size); 
+
+    $chunks = array(); 
+
+    $start = 0; 
+    for ($kk=0; $kk < $num_chunks; $kk++){ 
+      $chunks[] = substr($contents, $start, $chunk_size); //get $chunk_size bytes at a time 
+      $start += $chunk_size; 
+    } 
+     
+    if ($start < $size){ 
+       $chunks[] = substr($contents, $start);  //get any leftover 
+    } 
+    return $chunks; 
+}  
+
+	// Reading Map Settings
+	$settings = file_chop($_FILES['mapfile']['tmp_name'],1);
+	// Numeric Values
+	$maxplayers = ord($settings[9]);
+	$holdtime = ord($settings[10]);
+	$teams = ord($settings[11]);
+	$powerups = ord($settings[21]);
+	
+	// Enabled/Disabled values
+	$missile = ord($settings[16]);
+	$nade = ord($settings[17]);
+	$bouncy = ord($settings[18]);
+	
+	// High/Low
+	$laser = ord($settings[13]);
+	$special = ord($settings[14]);
+	$recharge = ord($settings[15]);
+	
+	// Game Type
+	$objective = ord($settings[12]);
+	
+	$namestart = 29;
+	$namelen = ord($settings[($namestart - 2)]);
+	for($i = ($namestart); $i < ($namestart + $namelen); $i++){
+		$bname .= $settings[$i];
+	}
+	$name = addslashes($name);
+	
+	$deslen = ord($settings[$i]);
+	$desstart = $i + 2;
+	for($i = $desstart; $i < ($desstart + $deslen); $i++){
+		$description .= $settings[$i];
+	}
+	$description = htmlentities($description, ENT_QUOTES);
+	// End of Settings Reading
+	$description = '';
+	
+	if($id == ""){
+		$name = strtolower($_FILES['mapfile']['name']);
+		$name = substr($name,0, strlen($name) - 4);
+	}else{
+		$name = strtolower(mysql_result(mysql_query("SELECT name FROM maps WHERE id='$id'"),0,'name'));
+	}
+
+	$name = addslashes($name);
